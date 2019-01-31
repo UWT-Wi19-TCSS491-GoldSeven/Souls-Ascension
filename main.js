0ed0d0a13859906f5ca5fbee02257d6192d26dcd
tom5862 <thomas5862@gmail.com>
2019-01-30 18:37:50 -0800

    Testing

--- main.js
+++ main.js
@@ -51,38 +51,71 @@ Animation.prototype.currentFrame = function() {
 Animation.prototype.isDone = function() {
     return (this.elapsedTime >= this.totalTime);
 }
-
-function Background(game) {
-    Entity.call(this, game, 0, 400);
-    this.radius = 200;
-}
+//----------------------------------------------------------------------------------------------------------------------------------------
+/* 
+ * Slime Dungeon Level 1 (88x33) each number is a 32x32 pixel space
+ * 0 = no block (should layer background image so these are not just a solid color)
+ * 1-4 = alternating horizontal wall tiles, 5 = Vertical wall tile, 6 = Top Left L shaped corner, 7 = Top Right L shaped corner,
+ * 8 = Bottom Left L shaped corner, 9 = Bottom Right L shaped corner, 10 = North T shaped wall, 11 = East T shaped wall,
+ * 12 = West T shaped wall, 13 = South T shaped wall, 14 = + shaped wall, 15 = Horizontal wall with door,
+ * 16 = North floor, 17 = East floor, 18 = West floor, 19 = South floor, 20 = Top Left L floor, 21 = Top Right L floor,
+ * 22 = Bottom Left L floor, 23 = Bottom Right L floor, 24 = Center floor.
+ */
+var slimeDungeonLevelOne = new Array(
+	0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,6 ,1 ,2 ,1 ,7 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,6 ,1 ,2 ,1 ,7 ,0 ,0 ,0 ,0 ,6 ,3 ,4 ,7 ,0 ,0 ,0 ,0 ,6 ,1 ,1 ,2 ,1 ,4 ,1 ,7 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+	0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,5 ,20,16,21,12,1 ,2 ,1 ,2 ,1 ,7 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,6 ,9 ,20,16,21,8 ,7 ,0 ,6 ,2 ,9 ,20,21,8 ,1 ,7 ,0 ,6 ,9 ,20,16,16,16,16,21,12,1 ,2 ,2 ,2 ,2 ,1 ,7 ,0 ,0 ,0 ,0 ,0 ,0 ,
+	0 ,0 ,0 ,0 ,6 ,1 ,2 ,1 ,1 ,2 ,2 ,1 ,3 ,4 ,1 ,1 ,1 ,2 ,2 ,1 ,1 ,1 ,3 ,1 ,1 ,2 ,1 ,11,18,24,17,5 ,0 ,0 ,0 ,0 ,0 ,8 ,7 ,0 ,0 ,0 ,6 ,1 ,2 ,1 ,10,10,4 ,9 ,20,24,24,24,21,8 ,10,9 ,20,16,24,24,16,21,8 ,2 ,11,20,24,24,19,19,19,23,5 ,20,16,16,16,16,21,8 ,3 ,1 ,4 ,7 ,0 ,0 ,
+	0 ,0 ,0 ,0 ,5 ,20,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,21,5 ,22,19,23,5 ,22,19,19,24,24,20,8 ,1 ,2 ,1 ,9 ,20,16,21,8 ,9 ,20,16,24,24,24,24,24,21,1 ,20,24,24,24,24,24,24,16,21,5 ,22,24,17,6 ,1 ,2 ,1 ,9 ,18,24,19,19,19,24,16,16,16,21,5 ,0 ,0 ,
+	0 ,0 ,0 ,0 ,5 ,18,24,24,24,24,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,24,17,8 ,3 ,15,4 ,13,1 ,1 ,7 ,18,24,24,16,16,16,16,16,24,24,24,16,16,24,19,24,24,24,24,24,24,16,24,24,24,24,24,24,24,24,17,8 ,7 ,18,17,5 ,20,16,16,16,24,23,6 ,10,7 ,18,24,24,24,17,5 ,0 ,0 ,
+	0 ,0 ,0 ,0 ,5 ,18,24,24,24,17,1 ,2 ,1 ,10,1 ,2 ,1 ,2 ,3 ,4 ,1 ,1 ,2 ,1 ,7 ,18,24,16,16,16,16,16,16,21,5 ,18,24,24,24,24,19,19,19,19,19,19,19,24,17,5 ,22,24,24,24,24,19,19,19,19,24,24,24,24,24,24,24,21,5 ,18,17,5 ,18,24,19,19,23,6 ,1 ,1 ,5 ,22,19,19,19,23,5 ,0 ,0 ,
+	0 ,0 ,0 ,0 ,5 ,18,24,24,24,24,16,16,21,5 ,20,16,16,16,16,16,16,16,16,21,5 ,18,24,24,24,24,24,24,24,23,5 ,18,24,24,24,17,6 ,10,10,10,1 ,1 ,7 ,18,17,8 ,7 ,18,24,24,17,1 ,10,10,7 
+	
+	);
+var currentScale = 32;
+var currentWTiles = 88; // number of tiles with wise on the map
+function Background(game, spritesheet) {
+    this.x = 0;
+    this.y = 0;
+	this.sw = 32;
+    this.sh = 32;
+	this.dw = currentScale;
+    this.dh = currentScale;
+    this.spritesheet = spritesheet;
+    this.game = game;
+    this.ctx = game.ctx;
+};
 
 Background.prototype = new Entity();
 Background.prototype.constructor = Background;
 
-Background.prototype.update = function() {}
+Background.prototype.update = function () {};
 
-Background.prototype.draw = function(ctx) {
-    ctx.fillStyle = "SaddleBrown";
-    ctx.fillRect(0, 500, 800, 300);
-    Entity.prototype.draw.call(this);
-}
+Background.prototype.draw = function () {
+	var spriteX = 0;
+	var spriteY = 0;
+	var count = 0;
+	var x = this.x;
+	var y = this.y;
+	
+	// Loop to generate each tile
+    for (var i = 0; i < slimeDungeonLevelOne.length -2; i++) {
+		spriteX = (slimeDungeonLevelOne[i] - 1) * 32; // 32 is the number of pixels per sprite
+		this.ctx.drawImage(this.spritesheet, spriteX, spriteY, this.sw, this.sh, x, y, this.dw, this.dh);
+		count++;
+		if (count >= currentWTiles) // change the value based on how many tiles you will draw. (88 atm)
+		{
+			x = this.x;
+			y += currentScale; 
+			count = 0;
+		}
+		else 
+		{
+			x += currentScale; 
+		}
+	};
+};
 
-function BlockThingy(game) {
-    Entity.call(this, game, 400, 0);
-    this.radius = 200;
-}
-
-BlockThingy.prototype = new Entity();
-BlockThingy.prototype.constructor = Background;
-
-BlockThingy.prototype.update = function() {}
-
-BlockThingy.prototype.draw = function(ctx) {
-    ctx.fillStyle = "red";
-    ctx.fillRect(0, 0, 20, 800);
-    Entity.prototype.draw.call(this);
-}
+//--------------------------------------------------------------------------------------------------
 
 function Samurai(game) {                                                                                               //loop   reversed
     this.alertAnimation = new Animation(ASSET_MANAGER.getAsset("./img/SamuraiHeavy_Alert.png"), 0, 0, 512, 512, 0.04, 9, true, false);
@@ -218,6 +251,7 @@ Samurai.prototype.draw = function(ctx) {
 
 var ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./img/DungeonBackgroundSpriteSheet.png");
 ASSET_MANAGER.queueDownload("./img/SamuraiHeavy_Alert.png");
 ASSET_MANAGER.queueDownload("./img/SamuraiHeavy_Attack.png");
 ASSET_MANAGER.queueDownload("./img/SamuraiHeavy_Die.png");
@@ -232,13 +266,11 @@ ASSET_MANAGER.downloadAll(function() {
     var ctx = canvas.getContext('2d');
 
     var gameEngine = new GameEngine();
-    var blockthingy = new BlockThingy(gameEngine); // Testing.
     var bg = new Background(gameEngine);
     var samurai = new Samurai(gameEngine);
 
-    gameEngine.addEntity(bg);
+	gameEngine.addEntity(bg);
     gameEngine.addEntity(samurai);
-    gameEngine.addEntity(blockthingy);
 
     gameEngine.init(ctx);
     gameEngine.start();
