d42a1b6f4040be58d211f97afbb45683c5c1cefe
Evan Lindsay <evanlindsay@live.com>
2019-02-23 16:04:05 -0800

    Copy muted state over to new audio instances

--- sound.js
+++ sound.js
@@ -4,12 +4,12 @@
 
 /**
  * An object to control sound effects.
- * @param {*} sourceFileName 
+ * @param {*} sourceFileName
  */
 function Sound(sourceFileName, repeat) {
     this._source = (sourceFileName) ? "sounds\\" + sourceFileName : null;
     this._audio = (this._source) ? new Audio(this._source) : null;
-    if(this._audio && repeat) {
+    if (this._audio && repeat) {
         this._audio.loop = repeat;
     }
 }
@@ -22,15 +22,15 @@ Sound.prototype = {
      *  https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/playbackRate
      */
     set playbackSpeed(speedPercent) {
-        this._audio.playbackRate = (speedPercent) ? 1.0 * (speedPercent/100.0) : 1.0
+        this._audio.playbackRate = (speedPercent) ? 1.0 * (speedPercent / 100.0) : 1.0
     },
     get playbackSpeed() {
         return this._audio.playbackRate
     }
 }
 
-Sound.prototype.play = function() {
-    if(this._source && this._audio) {
+Sound.prototype.play = function () {
+    if (this._source && this._audio) {
         this._audio.play();
     } else {
         console.error("Cannot play audio object.");
@@ -40,17 +40,19 @@ Sound.prototype.play = function() {
 /**
  * Allows for many repeats of this sound to overlap and play simultaneously. i.e. when pressing a key quickly several times during an attack.
  */
-Sound.prototype.replay = function() {
-    if(this._source && this._audio) {
-        this._audio = new Audio(this._source   );
+Sound.prototype.replay = function () {
+    if (this._source && this._audio) {
+        let temp = this._audio;
+        this._audio = new Audio(this._source);
+        this._audio.muted = temp.muted;
         this._audio.play();
     } else {
         console.error("Cannot play audio object.");
     }
 }
 
-Sound.prototype.pause = function() {
-    if(this._source && this._audio) {
+Sound.prototype.pause = function () {
+    if (this._source && this._audio) {
         this._audio.pause();
     } else {
         console.error("Cannot pause audio object.");
