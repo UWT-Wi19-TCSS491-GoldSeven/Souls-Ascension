9da1703c55ded41285bb196bf640da060d64b2f1
tom5862 <thomas5862@gmail.com>
2019-03-11 15:44:41 -0700

    made first level

--- js/levels/dungeon/1A.js
+++ js/levels/dungeon/1A.js
@@ -18,17 +18,26 @@ class Level1A extends Level {
     }
 
     prePopulate() {
-        this.queueTemplate(100, 100, Templates.roomTemplateSquare2);
-        this.queueTemplate(126, 92, Templates.roomTemplateSquare15)
-        this.queueTemplate(115, 100, Templates.hallwayTemplate1);
+        this.queueTemplate(1, 45, Templates.roomTemplateSquare2);
+		this.queueTemplate(22, 67, Templates.roomTemplateSquare3);
+		this.queueTemplate(45, 45, Templates.roomTemplateSquare4);
+		this.queueTemplate(22, 45, Templates.roomTemplateSquare13);
+		this.queueTemplate(22, 22, Templates.roomTemplateSquare21);
+        this.queueTemplate(22, 1, Templates.roomTemplateExitSquare1);
+		this.queueTemplate(14, 52, Templates.hallwayTemplate1);
+		this.queueTemplate(36, 52, Templates.hallwayTemplate1);
+		this.queueTemplate(28, 14, Templates.hallwayTemplate2);
+		this.queueTemplate(28, 36, Templates.hallwayTemplate2);
+        this.queueTemplate(28, 58, Templates.hallwayTemplate2);
+		
     }
 
     postPopulate() {
-        let player = new Player(this.game, 112 * this.tileDimension, 103 * this.tileDimension);
+        let player = new Player(this.game, this.fromColumn(28), this.fromRow(77));
 
         this.addEntity(player, 'Player');
-        this.addEntity(new Sorcerer(this.game, 112 * this.tileDimension, 112 * this.tileDimension));
-        this.addEntity(new Slime(this.game, 103 * this.tileDimension, 103 * this.tileDimension))
+        //this.addEntity(new Sorcerer(this.game, this.fromColumn(112), this.fromRow(112)));
+        //this.addEntity(new Slime(this.game, this.fromColumn(103), this.fromRow(103)))
     }
 
     isImpassable(id) {
