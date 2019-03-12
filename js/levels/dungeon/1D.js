afc6ae358e6cd328b1527a7001ad4cae8d6dcc7b
tom5862 <thomas5862@gmail.com>
2019-03-11 18:56:23 -0700

    Added boss level

--- js/levels/dungeon/1D.js
+++ js/levels/dungeon/1D.js
@@ -18,15 +18,25 @@ class Level1A extends Level {
     }
 
     prePopulate() {
-		this.queueTemplate(25, 73, Templates.roomTemplateSquare9);
+		this.queueTemplate(25, 73, Templates.roomTemplateSquare7);
 		this.queueTemplate(25, 49, Templates.roomTemplateSquare1);
-		this.queueTemplate(49, 49, Templates.roomTemplateSquare5);
-		this.queueTemplate(73, 49, Templates.roomTemplateSquare12);
-        this.queueTemplate(73, 25, Templates.roomTemplateExitSquare1);
+		this.queueTemplate(25, 25, Templates.roomTemplateSquare5);
+		this.queueTemplate(49, 73, Templates.roomTemplateSquare5);
+		this.queueTemplate(49, 49, Templates.roomTemplateSquare2);
+		this.queueTemplate(48, 25, Templates.roomTemplateSquare20);
+		this.queueTemplate(73, 73, Templates.roomTemplateSquare12);
+		this.queueTemplate(73, 49, Templates.roomTemplateSquare4);
+        this.queueTemplate(73, 25, Templates.roomTemplateSquare6);
+		this.queueTemplate(47, 97, Templates.roomTemplateBoss4);
 		this.queueTemplate(27, 63, Templates.hallwayTemplate2);
+		this.queueTemplate(27, 39, Templates.hallwayTemplate2);
+		this.queueTemplate(75, 63, Templates.hallwayTemplate2);
 		this.queueTemplate(39, 51, Templates.hallwayTemplate1);
 		this.queueTemplate(63, 51, Templates.hallwayTemplate1);
+		this.queueTemplate(63, 75, Templates.hallwayTemplate1);
+		this.queueTemplate(39, 27, Templates.hallwayTemplate1);
 		this.queueTemplate(75, 39, Templates.hallwayTemplate2);
+		this.queueTemplate(51, 87, Templates.hallwayTemplate2);
 		
     }
 
@@ -34,8 +44,8 @@ class Level1A extends Level {
         let player = new Player(this.game, this.fromColumn(33), this.fromRow(87));
 
         this.addEntity(player, 'Player');
-        this.addEntity(new Slime(this.game, this.fromColumn(28), this.fromRow(76)))
-		this.addEntity(new Slime(this.game, this.fromColumn(36), this.fromRow(76)))
+        //this.addEntity(new Sorcerer(this.game, this.fromColumn(112), this.fromRow(112)));
+        //this.addEntity(new Slime(this.game, this.fromColumn(103), this.fromRow(103)))
     }
 
     isImpassable(id) {
