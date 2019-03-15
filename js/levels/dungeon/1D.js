801b81470f4a5be0acbb5f59c20efeef34bc2e79
tom5862 <thomas5862@gmail.com>
2019-03-15 11:37:51 -0700

    Level 3 populated

--- js/levels/dungeon/1D.js
+++ js/levels/dungeon/1D.js
@@ -40,8 +40,88 @@ class Level1D extends DungeonLevel {
     }
 
     postPopulate() {
-        this.addEntity(new Sorcerer(this.game, this.fromColumn(33), this.fromRow(75)));
-        //this.addEntity(new Slime(this.game, this.fromColumn(103), this.fromRow(103)))a
+		// items
+		this.addEntity(new SoulJar(this.game, this.fromColumn(32), this.fromRow(57)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(33), this.fromRow(57)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(55), this.fromRow(31)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(55), this.fromRow(32)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(55), this.fromRow(33)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(55), this.fromRow(34)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(80), this.fromRow(56)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(80), this.fromRow(57)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(81), this.fromRow(56)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(81), this.fromRow(57)));
+		this.addEntity(new SoulJar(this.game, this.fromColumn(81), this.fromRow(81)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(32), this.fromRow(32)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(32), this.fromRow(33)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(33), this.fromRow(32)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(33), this.fromRow(33)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(56), this.fromRow(57)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(57), this.fromRow(57)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(80), this.fromRow(80)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(56), this.fromRow(80)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(56), this.fromRow(81)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(57), this.fromRow(80)));
+		this.addEntity(new HealingPotion(this.game, this.fromColumn(57), this.fromRow(81)));
+		this.addEntity(new GoldKey(this.game, this.fromColumn(80), this.fromRow(33)));
+		this.addEntity(new GoldKey(this.game, this.fromColumn(81), this.fromRow(32)));
+		this.addEntity(new SilverKey(this.game, this.fromColumn(80), this.fromRow(32)));
+		this.addEntity(new SilverKey(this.game, this.fromColumn(81), this.fromRow(33)));
+		
+		// enemies
+		this.addEntity(new Slime(this.game, this.fromColumn(30), this.fromRow(35)));
+		this.addEntity(new Slime(this.game, this.fromColumn(30), this.fromRow(39)));
+		this.addEntity(new Slime(this.game, this.fromColumn(35), this.fromRow(35)));
+		this.addEntity(new Slime(this.game, this.fromColumn(35), this.fromRow(39)));
+		this.addEntity(new Slime(this.game, this.fromColumn(55), this.fromRow(39)));
+		this.addEntity(new Slime(this.game, this.fromColumn(75), this.fromRow(35)));
+		this.addEntity(new Slime(this.game, this.fromColumn(85), this.fromRow(35)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(40), this.fromRow(32)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(40), this.fromRow(33)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(74), this.fromRow(50)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(75), this.fromRow(63)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(76), this.fromRow(50)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(77), this.fromRow(63)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(78), this.fromRow(50)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(83), this.fromRow(63)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(84), this.fromRow(50)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(85), this.fromRow(63)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(86), this.fromRow(50)));
+		this.addEntity(new SlimeBehemoth(this.game, this.fromColumn(87), this.fromRow(63)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(30), this.fromRow(50)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(35), this.fromRow(50)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(39), this.fromRow(55)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(39), this.fromRow(58)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(53), this.fromRow(30)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(53), this.fromRow(35)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(79), this.fromRow(31)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(79), this.fromRow(34)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(82), this.fromRow(31)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(82), this.fromRow(34)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(80), this.fromRow(76)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(81), this.fromRow(76)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(80), this.fromRow(85)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(81), this.fromRow(85)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(76), this.fromRow(80)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(76), this.fromRow(81)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(86), this.fromRow(80)));
+		this.addEntity(new Skeleton(this.game, this.fromColumn(86), this.fromRow(81)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(50), this.fromRow(32)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(50), this.fromRow(33)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(46), this.fromRow(32)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(46), this.fromRow(33)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(50), this.fromRow(60)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(55), this.fromRow(60)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(60), this.fromRow(60)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(50), this.fromRow(53)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(55), this.fromRow(53)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(60), this.fromRow(53)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(63), this.fromRow(56)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(63), this.fromRow(57)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(79), this.fromRow(38)));
+		this.addEntity(new Wraith(this.game, this.fromColumn(82), this.fromRow(38)));
+        this.addEntity(new Sorcerer(this.game, this.fromColumn(55), this.fromRow(110)));
+        this.addEntity(new Sorcerer(this.game, this.fromColumn(58), this.fromRow(110)));
     }
 }
 
